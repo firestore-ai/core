93ee2c3cbff6ab44d9cd86f3b0c0967e6660f4f9
henrik <houmingyuan@gmail.com>
2024-08-26 21:48:11 +0800

    fix compile

--- OOXML/Binary/Document/BinWriter/BinWriters.cpp
+++ OOXML/Binary/Document/BinWriter/BinWriters.cpp
@@ -2282,7 +2282,7 @@ void Binary_tblPrWriter::Write_tblpPr(const ComplexTypes::Word::CTblPPr& pr)
 		m_oBcw.WriteItemWithLengthEnd(nCurPos);
 	}
 }
-void Binary_tblPrWriter::WriteRowPr(const OOX::Logic::©Properties& rowPr)
+void Binary_tblPrWriter::WriteRowPr(const OOX::Logic::Properties& rowPr)
 {
 	int nCurPos = 0;
 	//CantSplit
